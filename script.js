35790c5f14c5fa15d6a176ef4f3fa37a166caf6a
negar <negarmemarjafari@gmail.com>
2023-06-10 17:20:26 +0430

    switch players

--- script.js
+++ script.js
@@ -16,7 +16,9 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 // rolling dice functionality
 btnRoll.addEventListener('click', function () {
@@ -31,8 +33,11 @@ btnRoll.addEventListener('click', function () {
   if (dice !== 1) {
     // Add dice to current score
     currentScore += dice;
-    current0El.textContent = currentScore; // change later
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+    //current0El.textContent = currentScore; // change later
   } else {
     // Switch to next player
+    activePlayer = activePlayer === 0 ? 1 : 0;
   }
 });
