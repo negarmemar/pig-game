7f5ab1d9ff66f21592bbbe784ef53de9b9f08966
negar <negarmemarjafari@gmail.com>
2023-06-10 17:29:56 +0430

    switch player 2

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 // selecting element
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
@@ -39,5 +41,9 @@ btnRoll.addEventListener('click', function () {
   } else {
     // Switch to next player
     activePlayer = activePlayer === 0 ? 1 : 0;
+    currentScore = 0;
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
 });
